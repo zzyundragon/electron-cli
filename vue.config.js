026dbed673d2224e60928954b836f929f7b07bb6
赵云龙 <yunlong.zhao@91wutong.com>
2019-10-17 11:27:30 +0800

    electron init

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+function resolve(dir) {
+	return path.join(__dirname, dir)
+}
+
+module.exports = {
+	publicPath: './',
+	devServer: {
+		host: '0.0.0.0',
+		port: 8080
+	},
+	chainWebpack: config => {
+		config.resolve.alias
+			.set('@', resolve('src'))
+			.set('src', resolve('src'))
+			.set('common', resolve('src/common'))
+			.set('components', resolve('src/components'));
+	}
+}
